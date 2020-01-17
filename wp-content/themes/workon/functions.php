f9ad5968cd32c4fe7a3d3d04ad553148953a1308
Domin6o <mail@wp.pl>
2020-01-17 12:11:52 +0100

    created theme files, assigned theme in wordpress panel

---
+++ wp-content/themes/workon/functions.php
@@ -0,0 +1,19 @@
+<?php
+    define("ROOT", get_template_directory_uri());
+    define("SCRIPTS", ROOT . "/scripts");
+    define("STYLES", ROOT . "/styles");
+    define("IMAGES", ROOT . "/images");
+
+    //add styles
+    function theme_styles() {
+        wp_enqueue_style("global", STYLES . "/css/global.min.css");
+    }
+
+    add_action("wp_enqueue_scripts", "theme_styles");
+
+    //add scripts
+    function theme_scripts() {
+        wp_enqueue_script("scripts", SCRIPTS . "/scripts.js", array("jquery", "1.0", true));
+    }
+
+    add_action("wp_enqueue_scripts", "theme_scripts");
