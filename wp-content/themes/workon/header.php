ee32fb92a16b0567d6640dc212097eea0065b17b
Domin6o <domin6o@wp.pl>
2020-01-21 13:25:31 +0100

    wordpress based navbars - LARGE FILE MERGE!

--- wp-content/themes/workon/header.php
+++ wp-content/themes/workon/header.php
@@ -6,10 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title> <?php bloginfo("nazwa"); wp_title(" | "); ?> </title>
 
-    <?php wp_head(); ?>
-    <!-- <link rel="stylesheet" href="<?php echo STYLES; ?>/css/global.min.css"> -->
-    <!-- <link rel="stylesheet" href="<?php echo ROOT; ?>/style.css"> -->
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+    <?php wp_head(); ?> <!-- dodanie styli na podstawie functions.php -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
     <link href="https://fonts.googleapis.com/css?family=Pathway+Gothic+One&display=swap&subset=latin-ext" rel="stylesheet">
 </head>
@@ -18,7 +15,14 @@
 
 <header>
     <nav class="nav-menu d-flex justify-content-end">
-		<ul>
+        <?php
+            wp_nav_menu([
+                "theme_location" => "header-nav-menu",
+                "container" => "ul",
+                "menu_class" => "list-inline"
+            ]); 
+        ?>
+		<!-- <ul>
             <li><a href="./index.html">Start</a><span class="mx-2"> | </spam></li>
             <li><a href="./about-us.html">About us</a><span class="mx-2"> | </spam></li>
             <li><a href="./offer.html">Offer</a><span class="mx-2"> | </spam></li>
@@ -26,7 +30,7 @@
             <li><a href="./portfolio.html">Portfolio</a><span class="mx-2"> | </spam></li>
             <li><a href="#">Blog</a><span class="mx-2"> | </spam></li>
             <li><a href="./contact.html">Contact us |</a><span class="mx-2"> | </spam></li>
-        </ul>
+        </ul> -->
     </nav>
 </header>
 
